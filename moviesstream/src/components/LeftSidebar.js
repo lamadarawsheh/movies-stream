6bb02f3924293119b7e93c7b376e09edb4d2b100
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-08-29 22:53:15 +0300

    feat: LeftSidebar IMPL - implement profile section and style it -implement users list  section and style it -implement leftsidebar footer  and style it

--- moviesstream/src/components/LeftSidebar.js
+++ moviesstream/src/components/LeftSidebar.js
@@ -1,9 +1,58 @@
-const LeftSidebar = () => {
-    return ( 
-        <div>
-            
+import { FaPlus } from "react-icons/fa6";
+import { LuLayoutGrid } from "react-icons/lu";
+import { MdOutlineExplore } from "react-icons/md";
+import { CiBookmark } from "react-icons/ci";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CiPlay1 } from "react-icons/ci";
+import { RiSettings2Line } from "react-icons/ri";
+import { MdOutlineLogout } from "react-icons/md";
+import ToggleSwitch from './Toggleswitch';
+import { ProfileImg ,UserImg} from "../assets";
+
+const LeftSidebar = ({Profile,Users}) => {
+    
+    return (
+        <div className ="left-sidebar">
+            <div className="profile-section">
+                    <button  className='profile-section-button-img'><img src={ProfileImg}></img></button>
+                    <button className="icon-button">
+                        <LuLayoutGrid className="Profile-section-icons" />
+                    </button>
+                    <button className="icon-button">
+                        <MdOutlineExplore className="Profile-section-icons" />
+                    </button>
+                    <button className="icon-button">
+                        <CiBookmark className="Profile-section-icons" />
+                    </button>
+                    <button className="icon-button">
+                        <IoIosNotificationsOutline className="Profile-section-icons" />
+                    </button>
+                    <button className="icon-button">
+                        <CiPlay1 className="Profile-section-icons" />
+                    </button>
+                    <button className="icon-button">
+                        <RiSettings2Line className="Profile-section-icons" />
+                    </button>
+
+
+
+                    
+            </div>
+            <div className="users-list">
+                    {Users.map((user)=>(
+                            <button className="user-button" key={user.id}><img src={UserImg}></img></button>
+                        
+                        ))}
+        
+                    <button className='add-user-button'><FaPlus /></button>
+            </div>
+            <div className="left-sidebar-footer">
+                <ToggleSwitch/>
+                <button className='logout-button'><MdOutlineLogout /></button>
+                
+            </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default LeftSidebar;
