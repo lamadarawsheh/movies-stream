4a02fcdb7135c6103c3be3ec210da6ad75919296
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-08-29 22:58:58 +0300

    feat: Rightsidebar IMPL -implement rightsidebar and style

--- moviesstream/src/components/RightSidebar.js
+++ moviesstream/src/components/RightSidebar.js
@@ -1,7 +1,36 @@
-const RightSidebar = () => {
-    return ( <div>
+
+const RightSidebar = ({mediaServices,Genres}) => {
+    return (
+        <div className="right-sidebar">
+            
+            <div className="mediaService-menu">
+                <h4>Media Service</h4>
+                {mediaServices.map((mediaService)=>(
+                <div className="mediaService-preview" key={mediaService.id}>   
+                    <div className="mediaService-item">
+                            <img src={mediaService.img} alt="Image 1" />
+                            <span>{mediaService.name}</span>
+                       
+                    </div>
+                </div>
+                ))}
+            </div>
+
+            <div className="Genre-menu">
+                <h4 >Genre</h4>
+                {Genres.map((Genre)=>(
+                <div className="Genre-preview" key={Genre.id}>   
+                        <div className="Genre-item">
+                            <a href="http://google.com">{Genre.name}</a>
+                        </div>
+                </div>
+                ))}
+            </div>
+          
+        </div>
+
         
-    </div> );
-}
- 
+    );
+};
+
 export default RightSidebar;
