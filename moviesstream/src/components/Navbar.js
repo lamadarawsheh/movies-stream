028a68f84360ce49c2d0c528d9800cd9fd8cde6e
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2024-08-29 23:03:46 +0300

    feat: Navbar IMP -implement navbar and style it -implement searchbar and style it

--- moviesstream/src/components/Navbar.js
+++ moviesstream/src/components/Navbar.js
@@ -1,7 +1,16 @@
+import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import SearchBar from "./SearchBar";
+
 const Navbar = () => {
-    return ( <div>
-        
-    </div> );
+    return ( 
+        <nav className="navbar">
+            <h4 className="navbar-header">Discover your favourites .</h4>
+            <div className="right-items">
+                <SearchBar/>
+                <button  className="settings-adjust"><HiOutlineAdjustmentsHorizontal /></button>
+            </div>
+        </nav>
+     );
 }
  
 export default Navbar;
